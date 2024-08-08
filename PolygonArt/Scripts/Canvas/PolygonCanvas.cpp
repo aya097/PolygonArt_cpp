cbbb7a45f016a0887f819a1dd1f4104314d3b1c8
aya097 <akase097@gmail.com>
2024-08-08 12:14:21 +0900

    add operate vertex

---
+++ PolygonArt/Scripts/Canvas/PolygonCanvas.cpp
@@ -0,0 +1,11 @@
+﻿#include "PolygonCanvas.h"
+
+void PolygonCanvas::AddVertex(Canvas::Vertex& const vertex)
+{
+	Vertices << vertex;
+}
+
+void PolygonCanvas::RemoveVertex(int const number)
+{
+	Vertices.erase(Vertices.begin() + number);
+}
---
+++ PolygonArt/Scripts/Canvas/PolygonCanvas.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include<Siv3D.hpp>
+
+namespace Canvas
+{
+	struct Vertex
+	{
+		Point Pos;
+	};
+
+	struct Polygon
+	{
+		int Index[3];
+		Color Color;
+	};
+}
+class PolygonCanvas
+{
+public:
+	void AddVertex(Canvas::Vertex& const vertex);
+	void RemoveVertex(int const number);
+private:
+	Array<Canvas::Vertex> Vertices;
+	Array<Canvas::Polygon> Polygons;
+};
