6a7c91212ec581f7b3404df8e66eb01635655e7e
aya097 <akase097@gmail.com>
2024-08-09 15:45:42 +0900

    add editor selector class

---
+++ PolygonArt/Scripts/Scene/Selector/EditorSelector.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "../PaintParams.h"
+#include "../Editor/Editor.h"
+#include "../Editor/VertexEditor.h"
+#include "../Editor/PolygonEditor.h"
+
+class EditorSelector
+{
+public:
+	EditorSelector();
+	const std::shared_ptr<Editor> SelectByPaintMode(const PaintMode& paintMode);
+private:
+	std::unordered_map<PaintMode, std::shared_ptr<Editor>> editors;
+};
