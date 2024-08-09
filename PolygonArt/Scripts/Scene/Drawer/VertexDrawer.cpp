2dfc6eba0b4c8b879ab2657273c1cffa18c1c6b0
aya097 <akase097@gmail.com>
2024-08-09 12:30:10 +0900

    apply decrator to drawer

--- PolygonArt/Scripts/Scene/Drawer/VertexDrawer.cpp
+++ PolygonArt/Scripts/Scene/Drawer/VertexDrawer.cpp
@@ -2,6 +2,12 @@
 
 void VertexDrawer::Draw(const PolygonCanvas& canvas)
 {
+	// 所有している Drawer の描画
+	if (drawer)
+	{
+		drawer->Draw(canvas);
+	}
+
 	// Vertex の描画
 	{
 		for (Canvas::Vertex v : canvas.GetVertices())
